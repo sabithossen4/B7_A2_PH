74d85d94d0fed74a06401353b00a238b7df14aa0
Sabit Hossen <sabithossen.dev@gmail.com>
2026-09-14 01:00:23 +0600

    app and service file configare

---
+++ src/db/index.ts
@@ -0,0 +1,27 @@
+import { Pool } from "pg";
+import config from "../config";
+
+export const pool = new Pool({
+  connectionString: config.connection_string,
+})
+
+export const initDB = async () => {
+  try {
+    await pool.query(`
+        CREATE TABLE if NOT EXISTS userS (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(20),
+        email VARCHAR(50) UNIQUE NOT NULL,
+        password VARCHAR(20) NOT NULL,
+        is_active BOOLEAN DEFAULT true,
+        age INT,
+
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )
+        `)
+    console.log("Table Created Successfully");
+  } catch (error) {
+    console.log(error);
+  }
+}
