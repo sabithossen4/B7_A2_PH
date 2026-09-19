c1897ccc3bf3dff5e49138f0d01185ea07d48f75
Sabit Hossen <sabithossen.dev@gmail.com>
2026-09-19 14:10:48 +0600

    auth update

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,19 +1,54 @@
 import type { NextFunction, Request, Response } from "express";
+import  jwt, { type JwtPayload }  from "jsonwebtoken";
+import config from "../config";
+import { pool } from "../db";
 
-const auth = () =>{
-  return async  (req : Request,res : Response, next : NextFunction) =>{
+const auth = () => {
+  return async (req: Request, res: Response, next: NextFunction) => {
     console.log("This is protected Route")
     console.log(req.headers.authorization);
     const token = req.headers.authorization;
 
-    if(!token){
-         res.status(401).json({
-      success: false,
-      message: "Unauthorized acces!!"
-    });
+    console.log(token)
+
+    if (!token) {
+      res.status(401).json({
+        success: false,
+        message: "Unauthorized acces!!"
+      });
     }
+
+    // verify a token symmetric - synchronous
+    var decoded = jwt.verify(token as string, config.secret as string) as JwtPayload;
+    // console.log(decoded)
+
+    const userData = await pool.query(`
+      SELECT * FROM users WHERE email=$1
+      `,
+      [decoded.email]
+    )
+    // console.log(userData);
+
+    const user = userData.rows[0];
+    // console.log(user);
+
+
+    if(userData.rows.length ===0){
+      res.status(404).json({
+        success: false,
+        message: "User not found"
+      })
+    }
+
+    if(!user.is_active) {
+      res.status(403).json({
+        success: false,
+        message: "Forbeden!!"
+      })
+    }
+
     next();
-}
+  }
 }
 
 export default auth
